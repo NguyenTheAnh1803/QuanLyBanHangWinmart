7f826c1685fd81d3957b92ec2508e0b405ba2dbf
NguyenTheAnh1803 <107667343+NguyenTheAnh1803@users.noreply.github.com>
2024-04-04 01:03:27 +0700

    Add project files.

---
+++ QuanLyBanHangWinmart/PresentationLayer/frmDoiMatKhau.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace QuanLyBanHangWinmart.PresentationLayer
+{
+    partial class frmDoiMatKhau
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label labelNhaplaimatkhaumoi;
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnDoiMatKhau = new System.Windows.Forms.Button();
+            this.labelUserName = new System.Windows.Forms.Label();
+            this.labelMatKhau = new System.Windows.Forms.Label();
+            this.labelMatKhauMoi = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtMatKhauMoi = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.txtNhaplaimatkhaumoi = new System.Windows.Forms.TextBox();
+            this.labelDoiMatKhau = new System.Windows.Forms.Label();
+            labelNhaplaimatkhaumoi = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(205, 330);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(150, 40);
+            this.btnThoat.TabIndex = 0;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnDoiMatKhau
+            // 
+            this.btnDoiMatKhau.Location = new System.Drawing.Point(394, 330);
+            this.btnDoiMatKhau.Name = "btnDoiMatKhau";
+            this.btnDoiMatKhau.Size = new System.Drawing.Size(150, 40);
+            this.btnDoiMatKhau.TabIndex = 1;
+            this.btnDoiMatKhau.Text = "Đổi mật khẩu";
+            this.btnDoiMatKhau.UseVisualStyleBackColor = true;
+            this.btnDoiMatKhau.Click += new System.EventHandler(this.btnDoiMatKhau_Click);
+            // 
+            // labelUserName
+            // 
+            this.labelUserName.AutoSize = true;
+            this.labelUserName.Location = new System.Drawing.Point(174, 123);
+            this.labelUserName.Name = "labelUserName";
+            this.labelUserName.Size = new System.Drawing.Size(75, 17);
+            this.labelUserName.TabIndex = 2;
+            this.labelUserName.Text = "UserName";
+            this.labelUserName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelMatKhau
+            // 
+            this.labelMatKhau.AutoSize = true;
+            this.labelMatKhau.Location = new System.Drawing.Point(174, 175);
+            this.labelMatKhau.Name = "labelMatKhau";
+            this.labelMatKhau.Size = new System.Drawing.Size(68, 17);
+            this.labelMatKhau.TabIndex = 3;
+            this.labelMatKhau.Text = "Mật Khẩu";
+            // 
+            // labelMatKhauMoi
+            // 
+            this.labelMatKhauMoi.AutoSize = true;
+            this.labelMatKhauMoi.Location = new System.Drawing.Point(174, 225);
+            this.labelMatKhauMoi.Name = "labelMatKhauMoi";
+            this.labelMatKhauMoi.Size = new System.Drawing.Size(98, 17);
+            this.labelMatKhauMoi.TabIndex = 4;
+            this.labelMatKhauMoi.Text = "Mật Khẩu mới ";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(305, 123);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(239, 22);
+            this.txtUserName.TabIndex = 5;
+            // 
+            // txtMatKhauMoi
+            // 
+            this.txtMatKhauMoi.Location = new System.Drawing.Point(305, 225);
+            this.txtMatKhauMoi.Name = "txtMatKhauMoi";
+            this.txtMatKhauMoi.Size = new System.Drawing.Size(239, 22);
+            this.txtMatKhauMoi.TabIndex = 6;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(305, 172);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(239, 22);
+            this.txtMatKhau.TabIndex = 7;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // labelNhaplaimatkhaumoi
+            // 
+            labelNhaplaimatkhaumoi.AutoSize = true;
+            labelNhaplaimatkhaumoi.Location = new System.Drawing.Point(174, 273);
+            labelNhaplaimatkhaumoi.Name = "labelNhaplaimatkhaumoi";
+            labelNhaplaimatkhaumoi.Size = new System.Drawing.Size(122, 17);
+            labelNhaplaimatkhaumoi.TabIndex = 8;
+            labelNhaplaimatkhaumoi.Text = "Nhập lại mật khẩu";
+            labelNhaplaimatkhaumoi.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // txtNhaplaimatkhaumoi
+            // 
+            this.txtNhaplaimatkhaumoi.Location = new System.Drawing.Point(305, 273);
+            this.txtNhaplaimatkhaumoi.Name = "txtNhaplaimatkhaumoi";
+            this.txtNhaplaimatkhaumoi.Size = new System.Drawing.Size(239, 22);
+            this.txtNhaplaimatkhaumoi.TabIndex = 9;
+            // 
+            // labelDoiMatKhau
+            // 
+            this.labelDoiMatKhau.AutoSize = true;
+            this.labelDoiMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.labelDoiMatKhau.Location = new System.Drawing.Point(243, 55);
+            this.labelDoiMatKhau.Name = "labelDoiMatKhau";
+            this.labelDoiMatKhau.Size = new System.Drawing.Size(217, 39);
+            this.labelDoiMatKhau.TabIndex = 10;
+            this.labelDoiMatKhau.Text = "Đổi mật khẩu";
+            // 
+            // frmDoiMatKhau
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.labelDoiMatKhau);
+            this.Controls.Add(this.txtNhaplaimatkhaumoi);
+            this.Controls.Add(labelNhaplaimatkhaumoi);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtMatKhauMoi);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.labelMatKhauMoi);
+            this.Controls.Add(this.labelMatKhau);
+            this.Controls.Add(this.labelUserName);
+            this.Controls.Add(this.btnDoiMatKhau);
+            this.Controls.Add(this.btnThoat);
+            this.Name = "frmDoiMatKhau";
+            this.Text = "frmDoiMatKhau";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnDoiMatKhau;
+        private System.Windows.Forms.Label labelUserName;
+        private System.Windows.Forms.Label labelMatKhau;
+        private System.Windows.Forms.Label labelMatKhauMoi;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtMatKhauMoi;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox txtNhaplaimatkhaumoi;
+        private System.Windows.Forms.Label labelDoiMatKhau;
+    }
+}
---
+++ QuanLyBanHangWinmart/PresentationLayer/frmDoiMatKhau.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLyBanHangWinmart.PresentationLayer
+{
+    public partial class frmDoiMatKhau : Form
+    {
+        public frmDoiMatKhau()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        SqlConnection cn = new SqlConnection(@"Data Source=DESKTOP-JCE3KO8\SQLEXPRESS;Initial Catalog=QuanLyWinMart;Integrated Security=True");
+
+        private void btnDoiMatKhau_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cn.Open();
+
+                string queryCheckUser = "SELECT COUNT(*) FROM tblTaiKhoan WHERE sTenDangNhap = @UserName AND sMatKhau = @OldPassword";
+                SqlCommand cmdCheckUser = new SqlCommand(queryCheckUser, cn);
+                cmdCheckUser.Parameters.AddWithValue("@UserName", txtUserName.Text);
+                cmdCheckUser.Parameters.AddWithValue("@OldPassword", txtMatKhau.Text);
+
+                int userCount = (int)cmdCheckUser.ExecuteScalar();
+
+                if (userCount == 1)
+                {
+                    if (txtMatKhauMoi.Text == txtNhaplaimatkhaumoi.Text)
+                    {
+                        string queryUpdatePassword = "UPDATE tblTaiKhoan SET sMatKhau = @NewPassword WHERE sTenDangNhap = @UserName AND sMatKhau = @OldPassword";
+                        SqlCommand cmdUpdatePassword = new SqlCommand(queryUpdatePassword, cn);
+                        cmdUpdatePassword.Parameters.AddWithValue("@NewPassword", txtMatKhauMoi.Text);
+                        cmdUpdatePassword.Parameters.AddWithValue("@UserName", txtUserName.Text);
+                        cmdUpdatePassword.Parameters.AddWithValue("@OldPassword", txtMatKhau.Text);
+
+                        cmdUpdatePassword.ExecuteNonQuery();
+
+                        MessageBox.Show("Đổi mật khẩu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        this.Hide();
+                        frmMain frm = new frmMain();
+                        frm.ShowDialog();
+                        Close();
+
+
+                    }
+                    else
+                    {
+                        errorProvider1.SetError(txtNhaplaimatkhaumoi, "Mật khẩu nhập lại không khớp");
+                    }
+                }
+                else
+                {
+                    errorProvider1.SetError(txtUserName, "Tên người dùng hoặc mật khẩu không đúng");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cn.Close();
+               
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmMain frm = new frmMain();
+            frm.ShowDialog();
+            Close();
+            this.Close();
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
